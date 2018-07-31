b53aef6894097d6588efa8e94ca0f34a7c96e3b2
nelsongonzalezpo <nelson.gonzalezpo@udlap.mx>
2018-07-31 12:04:43 -0500

    Adding 20-21

---
+++ LPTHW-1/19.py
@@ -0,0 +1,21 @@
+#Ejercicio 19
+
+def cheese_and_crackers(cheese_count, boxes_of_crackers):
+    print "You have %d cheeses" %cheese_count
+    print "You have %d boxes" %boxes_of_crackers
+
+    print "Enough for a party"
+
+
+cheese_and_crackers(10,20)
+
+print "End of our program..."
+
+print "We are doing this in a different way"
+
+amount_of_boxes = 200
+amount_of_cheese = 1000
+
+cheese_and_crackers(amount_of_cheese, amount_of_boxes)
+
+print "Now is the end..."
